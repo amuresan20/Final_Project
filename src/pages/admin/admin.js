3431b3d2ddacec340a98eba2011149c7e679f6f3
Andreea Muresan <126514565+amuresan20@users.noreply.github.com>
2023-06-27 00:42:58 +0300

    update 2

--- src/pages/admin/admin.js
+++ src/pages/admin/admin.js
@@ -1,207 +1,148 @@
 import { createProductTableRow } from "../../components/productTableRow";
 import { getProducts } from "../../api/getProducts";
+import { getProductById } from "../../api/getProductById";
 import { deleteProductById } from "../../api/deleteProductById";
-
- const image = document.getElementById("image");
-  const name = document.getElementById("name");
-  const details = document.getElementById("description");
-  const stock = document.getElementById("stock");
-  const price = document.getElementById("price");
+import { updateProductById } from "../../api/updateProductById";
+import { insertProduct } from "../../api/insertProduct";
 
 const tableBody = document.getElementById("products-table-body");
-let isEditing = false;
-let addingNewProduct = false;
-let productId = 0;
+const addButon = document.getElementById("add-new-product");
+const formular = document.getElementById("modif-add-form");
+const tabel = document.getElementById("administrare");
+const salvare = document.getElementById("save-form");
+const anulare = document.getElementById("undo-form");
 
 window.addEventListener("DOMContentLoaded", () => {
   let isLoading = true;
 
   const spinnerHTML = `<div style="padding-left:40vw">
-		<img src="https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif?20170503175831" />
-		</div>
-		`;
+  <img src="https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/200w.gif?cid=6c09b952zqmvf8hi0l60tc39bsamr2i6qhnu5r0918kcglsv&ep=v1_gifs_search&rid=200w.gif&ct=g" />
+  </div>
+  `;
 
   getProducts().then((products) => {
     isLoading = false;
     const tableRows = products
+      .sort((a, b) => (a.name > b.name ? 1 : -1))
       .map((product) => createProductTableRow(product))
       .join("");
     tableBody.innerHTML = tableRows;
-    const allProductNames = document.querySelectorAll(".product-name");
-    allProductNames.forEach((name) => {
-      name.addEventListener("click", (event) => {
-        event.preventDefault();
-        isEditing = true;
-        addingNewProduct = false;
-        const form = document.querySelector(".form-admin");
-        form.style.display = "block";
-        const table = document.querySelector(".table-wrapper");
-        table.style.display = "none";
-        productId = event.target.parentNode.parentNode.id.substring(1);
-        const nameValue = event.target.textContent;
-        const stockValue =
-          event.target.parentElement.parentElement.querySelector(
-            ".product-stock"
-          ).textContent;
-        const imageValue = event.target.parentElement.parentElement
-          .querySelector(".admin-image")
-          .getAttribute("src");
-        const priceValue =
-          event.target.parentElement.parentElement.querySelector(
-            ".product-price"
-          ).textContent;
-
-        const inputName = document.querySelector("#name");
-        inputName.value = nameValue;
-        const inputPrice = document.querySelector("#price");
-        inputPrice.value = priceValue;
-        const inputStock = document.querySelector("#stock");
-        inputStock.value = stockValue;
-        const inputImage = document.querySelector("#image");
-        inputImage.value = imageValue;
-      });
-    });
-
-    if (isLoading) {
-      tableBody.innerHTML = spinnerHTML;
-    }
   });
+
+  if (isLoading) {
+    tableBody.innerHTML = spinnerHTML;
+  }
 });
 
 tableBody.addEventListener("click", onClick);
+addButon.addEventListener("click", onClick2);
+salvare.addEventListener("click", onClick3);
+anulare.addEventListener("click", onClick4);
+
+const idProdus = document.getElementById("id-produs");
+const numeProdus = document.getElementById("nume-produs");
+const imagineProdus = document.getElementById("imagine-produs");
+const cantitateProdus = document.getElementById("cantitate-produs");
+const pretProdus = document.getElementById("pret-produs");
 
 async function onClick(e) {
+  const id = e.target.parentNode.parentNode.id.substring(1);
+  //sterge produs
   if (e.target.classList.contains("delete-product")) {
-    //const id = e.target.parentNode.parentNode.id.substring(1);
+    // id format of the button is p23=p+product.id
     e.target.parentNode.parentNode.remove();
     const response = await deleteProductById(id);
-    console.log(response);
+  }
+  //apeleaza formularul de modificare produs
+  if (e.target.classList.contains("modify-product")) {
+    tabel.style.display = "none";
+
+    const product = await getProductById(id);
+    idProdus.innerHTML = `#${id}`;
+    imagineProdus.value = product.image;
+    numeProdus.value = product.name;
+    cantitateProdus.value = product.quantity;
+    pretProdus.value = product.price;
+
+    document.querySelector("#context-form h2").innerHTML = "Modifica produs";
+    formular.style.display = "block";
   }
 }
 
-const addNewProductButton = document.querySelector(".add-new-product");
-addNewProductButton.addEventListener("click", () => {
-  addingNewProduct = true;
-  isEditing = false;
-  const form = document.querySelector(".form-admin");
-  form.style.display = "block";
-  const table = document.querySelector(".table-wrapper");
-  table.style.display = "none";
-});
+async function onClick2(e) {
+  //apeleaza formularul de creare produs nou
+  tabel.style.display = "none";
 
-const submitButton = document.querySelector(".submit");
-submitButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  
+  idProdus.innerHTML = "#";
+  imagineProdus.value = "";
+  numeProdus.value = "";
+  cantitateProdus.value = "";
+  pretProdus.value = "";
+  document.querySelector("#context-form h2").innerHTML =
+    "Introducere produs nou";
+  formular.style.display = "block";
+}
 
-  if (addingNewProduct) {
-    const productsData = {
-      image,
-      name,
-      details,
-      stock,
-      price,
-    };
+async function onClick3() {
+  //insereaza produs
+  if (document.getElementById("id-produs").innerHTML === "#") {
+    formular.style.display = "none";
 
-    fetch("https://6490add11e6aa71680cba5af.mockapi.io/produse/", 
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productsData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const form = document.querySelector(".form-admin");
-        form.style.display = "none";
-        const table = document.querySelector(".table-wrapper");
-        table.style.display = "block";
-
-        getProducts().then((products) => {
-          const tableRows = products
-            .map((product) => createProductTableRow(product))
-            .join("");
-          tableBody.innerHTML = tableRows;
-          const allProductNames = document.querySelectorAll(".product-name");
-          allProductNames.forEach((name) => {
-            isEditing = true;
-            addingNewProduct = false;
-            name.addEventListener("click", (event) => {
-              event.preventDefault();
-              const form = document.querySelector(".form-admin");
-              form.style.display = "block";
-              const table = document.querySelector(".table-wrapper");
-              table.style.display = "none";
-            });
-          });
-        });
-
-        document.getElementById("image").value = "";
-        document.getElementById("name").value = "";
-        document.getElementById("description").value = "";
-        document.getElementById("stock").value = "";
-        document.getElementById("price").value = "";
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    addingNewProduct = false;
+    const response = await insertProduct(
+      numeProdus.value,
+      imagineProdus.value,
+      Number(pretProdus.value),
+      cantitateProdus.value,
+      pretProdus.value
+    );
+    tableBody.innerHTML += createProductTableRow(response);
+    tabel.style.display = "block";
+    console.log(response);
   }
+  //actualizeaza produs
+  else {
+    formular.style.display = "none";
+    console.log("aici");
 
-  if (isEditing) {
-    const updatedMaterialsData = {
-      image,
-      name,
-      details,
-      stock,
-      price,
-    };
+    const response = await updateProductById(
+      idProdus.innerHTML.substring(1),
+      numeProdus.value,
+      imagineProdus.value,
+      pretProdus.value,
+      cantitateProdus.value
+    );
+    const liniaCurenta = document.getElementById(
+      "p" + idProdus.innerHTML.substring(1)
+    );
+    liniaCurenta.querySelector(
+      "td:nth-of-type(2)"
+    ).innerHTML = `<img src=${imagineProdus.value} height="40px"/>`;
+    liniaCurenta.querySelector("td:nth-of-type(3)").innerHTML =
+      numeProdus.value;
+    liniaCurenta.querySelector("td:nth-of-type(4)").innerHTML =
+      pretProdus.value + " Lei";
+    liniaCurenta.querySelector("td:nth-of-type(5)").innerHTML =
+      cantitateProdus.value;
 
-    fetch(`https://6490add11e6aa71680cba5af.mockapi.io/produse/${productId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedProductData),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const form = document.querySelector(".form-admin");
-        form.style.display = "none";
-        const table = document.querySelector(".table-wrapper");
-        table.style.display = "block";
+    liniaCurenta.querySelector(
+      "td:nth-of-type(6)"
+    ).innerHTML = `<button class='modify-product'>Editare</button>`;
+    liniaCurenta.querySelector(
+      "buton.delete-product"
+    ).innerHTML = `<button class='delete-product'>Stergere</button>`;
 
-        getProducts().then((products) => {
-          const tableRows = products
-            .map((product) => createProductTableRow(product))
-            .join("");
-          tableBody.innerHTML = tableRows;
-          const allProductNames = document.querySelectorAll(".product-name");
-          allProductNames.forEach((name) => {
-            name.addEventListener("click", (event) => {
-              event.preventDefault();
-              isEditing = true;
-              addingNewProduct = false;
-              const form = document.querySelector(".form-admin");
-              form.style.display = "block";
-              const table = document.querySelector(".table-wrapper");
-              table.style.display = "none";
-            });
-          });
-        });
+    idProdus.innerHTML = "#";
+    imagineProdus.value = "";
+    numeProdus.value = "";
+    pretProdus.value = "";
+    cantitateProdus.value = "";
 
-        document.getElementById("image").value = "";
-        document.getElementById("name").value = "";
-        document.getElementById("description").value = "";
-        document.getElementById("stock").value = "";
-        document.getElementById("price").value = "";
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    isEditing = false;
+    tabel.style.display = "block";
   }
-});
+}
+
+async function onClick4() {
+  //butonul de cancel
+  formular.style.display = "none";
+  tabel.style.display = "block";
+}
